39bbe0d3c6bc3e38976162318cf2e8d2b729125a
Serhat Kaçmaz <serhatkacmaz3@gmail.com>
2023-03-25 19:27:25 +0300

    Add project files.

---
+++ BlogSite.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlogSite.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
