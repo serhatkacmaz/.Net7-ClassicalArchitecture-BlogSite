c31ed9e055e68b3fb9bee9fd356ad5ea9590a2b2
Serhat Kaçmaz <serhatkacmaz3@gmail.com>
2023-03-26 00:21:30 +0300

    IGenericRepository - IService - IUnitOfWork by skacmaz

---
+++ BlogSite.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSite.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetAsync(int id);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        
+        void Updates(T entity); 
+        
+        void Deletes(T entity);
+        void DeletesRange(IEnumerable<T> entities);
+
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+    }
+}
---
+++ BlogSite.Core/Services/IService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSite.Core.Services
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+
+        Task UpdatesAsync(T entity);
+
+        Task DeletesAsync(T entity);
+        Task DeletesRangeAsync(IEnumerable<T> entities);
+
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+    }
+}
