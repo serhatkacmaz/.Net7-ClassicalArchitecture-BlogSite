21ad951e4b61380d8acba13e0f53490aa2c5bd86
Serhat Kaçmaz <serhatkacmaz3@gmail.com>
2023-04-09 18:15:37 +0300

    user operations by skacmaz

---
+++ BlogSite.API/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using BlogSite.API.Filters;
+using BlogSite.Core.DTOs.UserBase;
+using BlogSite.Core.Entities.UserBase;
+using BlogSite.Core.Services;
+using BlogSite.Service.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogSite.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : BaseController
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return CreateActionResult(await _userService.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        [ServiceFilter(typeof(NotFoundFilter<User, UserDto>))]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return CreateActionResult(await _userService.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(UserDto userDto)
+        {
+            return CreateActionResult(await _userService.AddAsync(userDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UserDto userDto)
+        {
+            return CreateActionResult(await _userService.UpdateAsync(userDto));
+        }
+
+        [HttpDelete("{id}")]
+        [ServiceFilter(typeof(NotFoundFilter<User, UserDto>))]
+        public async Task<IActionResult> Remove(int id)
+        {
+            return CreateActionResult(await _userService.RemoveAsync(id));
+        }
+
+    }
+}
---
+++ BlogSite.Service/Services/UserService.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using BlogSite.Core.DTOs.UserBase;
+using BlogSite.Core.Entities.UserBase;
+using BlogSite.Core.Repositories;
+using BlogSite.Core.Services;
+using BlogSite.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSite.Service.Services
+{
+    public class UserService : Service<User, UserDto>, IUserService
+    {
+        public UserService(IGenericRepository<User> repository, IUnitOfWork unitOfWork, IMapper mapper) : base(repository, unitOfWork, mapper)
+        {
+        }
+    }
+}
