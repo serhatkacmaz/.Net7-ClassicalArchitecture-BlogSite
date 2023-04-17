b0bee78c819d517d9780852cc369536d1bb10a72
Serhat Kaçmaz <serhatkacmaz3@gmail.com>
2023-04-17 17:37:22 +0300

    login, register css, html js by skacmaz

---
+++ BlogSite.Web/wwwroot/js/loginRegister.js
@@ -0,0 +1,25 @@
+﻿//-> Login Active Panel
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+
+//-> login Modal Click
+function closeLoginModal() {
+	document.getElementById("r_Name").value = "";
+	document.getElementById("r_Email").value = "";
+	document.getElementById("r_Password").value = "";
+
+	document.getElementById("l_Email").value = "";
+	document.getElementById("l_Password").value = "";
+}
+
+document.getElementById("loginRegisterBtn").addEventListener("click", closeLoginModal);
