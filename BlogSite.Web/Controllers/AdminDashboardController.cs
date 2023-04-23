8f1f913b69b4dbf5f740e1db58b490ce68218508
Serhat Kaçmaz <serhatkacmaz3@gmail.com>
2023-04-23 16:05:49 +0300

    user

--- BlogSite.Web/Controllers/AdminDashboardController.cs
+++ BlogSite.Web/Controllers/AdminDashboardController.cs
@@ -5,21 +5,22 @@ using BlogSite.Web.Helpers;
 using BlogSite.Web.Models.AdminDashboard;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BlogSite.Web.Controllers
 {
     [Authorize(Roles = "Admin")]
     public class AdminDashboardController : Controller
     {
-        private readonly UserApiService _adminDashboardApiService;
+        private readonly UserApiService _userApiService;
         private readonly BlogApiService _blogApiService;
         private readonly MovementApiService _movementApiService;
         private readonly CategoryApiService _categoryApiService;
         private readonly RoleApiService _roleApiService;
 
-        public AdminDashboardController(UserApiService adminDashboardApiService, BlogApiService blogApiService, MovementApiService movementApiService, CategoryApiService categoryApiService, RoleApiService roleApiService)
+        public AdminDashboardController(UserApiService userApiService, BlogApiService blogApiService, MovementApiService movementApiService, CategoryApiService categoryApiService, RoleApiService roleApiService)
         {
-            _adminDashboardApiService = adminDashboardApiService;
+            _userApiService = userApiService;
             _blogApiService = blogApiService;
             _movementApiService = movementApiService;
             _categoryApiService = categoryApiService;
@@ -29,7 +30,7 @@ namespace BlogSite.Web.Controllers
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var userCountTask = _adminDashboardApiService.GetUserCountAsync();
+            var userCountTask = _userApiService.GetUserCountAsync();
             var viewCountTask = _blogApiService.GetTotalViewCountAsync();
             var totalCountTask = _blogApiService.GetTotalCountAsync();
             var likeCountTask = _movementApiService.GetTotalBlogLikeCountAsync();
@@ -159,5 +160,58 @@ namespace BlogSite.Web.Controllers
         }
         #endregion
 
+        #region User
+        [HttpGet]
+        public async Task<IActionResult> UserGrid()
+        {
+            return View(await _userApiService.GetAllAsync());
+        }
+
+        [HttpGet]
+        public IActionResult CreateNewUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNewUser(UserDto userDto)
+        {
+            try
+            {
+                var responseDto = await _userApiService.SaveAsync(userDto);
+                ErrorHelper.ResponseHandler(responseDto, this.ControllerContext);
+
+                return RedirectToAction(nameof(UserGrid));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UserEdit(int id)
+        {
+            return View(await _userApiService.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserEdit(UserDto userDto)
+        {
+            try
+            {
+                var result = await _userApiService.UpdateAsync(userDto);
+
+                if (!result)
+                    return View();
+                else
+                    return RedirectToAction(nameof(UserGrid));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        #endregion
     }
 }
