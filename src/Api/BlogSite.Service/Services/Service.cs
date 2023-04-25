234042158e06090fedc4c8ed8e2a921c8d0cc07c
Serhat Kaçmaz <serhatkacmaz3@gmail.com>
2023-04-25 23:42:58 +0300

    last change

--- src/Api/BlogSite.Service/Services/Service.cs
+++ src/Api/BlogSite.Service/Services/Service.cs
@@ -13,69 +13,6 @@ namespace BlogSite.Service.Services
 {
     public class Service<Entity, Dto> : IService<Entity, Dto> where Entity : BaseEntity where Dto : class
     {
-        #region Old Method
-        //private readonly IGenericRepository<T> _repository;
-        //private readonly IUnitOfWork _unitOfWork;
-
-        //public Service(IGenericRepository<T> repository, IUnitOfWork unitOfWork)
-        //{
-        //    _repository = repository;
-        //    _unitOfWork = unitOfWork;
-        //}
-
-        //public async Task<T> AddAsync(T entity)
-        //{
-        //    await _repository.AddAsync(entity);
-        //    await _unitOfWork.CommitAsync();
-        //    return entity;
-        //}
-
-        //public async Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities)
-        //{
-        //    await _repository.AddRangeAsync(entities);
-        //    await _unitOfWork.CommitAsync();
-        //    return entities;
-        //}
-
-        //public async Task<bool> AnyAsync(Expression<Func<T, bool>> expression)
-        //{
-        //    return await _repository.AnyAsync(expression);
-        //}
-
-        //public async Task RemoveAsync(T entity)
-        //{
-        //    _repository.Remove(entity);
-        //    await _unitOfWork.CommitAsync();
-        //}
-
-        //public async Task RemoveRangeAsync(IEnumerable<T> entities)
-        //{
-        //    _repository.RemoveRange(entities);
-        //    await _unitOfWork.CommitAsync();
-        //}
-
-        //public async Task<IEnumerable<T>> GetAllAsync()
-        //{
-        //    return await _repository.GetAll().ToListAsync();
-        //}
-
-        //public async Task UpdateAsync(T entity)
-        //{
-        //    _repository.Update(entity);
-        //    await _unitOfWork.CommitAsync();
-        //}
-
-        //public IQueryable<T> Where(Expression<Func<T, bool>> expression)
-        //{
-        //    return _repository.Where(expression);
-        //}
-
-        //public async Task<T> GetByIdAsync(object id)
-        //{
-        //    return await _repository.GetByIdAsync(id);
-        //}
-        #endregion
-
         private readonly IGenericRepository<Entity> _repository;
         protected readonly IUnitOfWork _unitOfWork;
         protected readonly IMapper _mapper;
