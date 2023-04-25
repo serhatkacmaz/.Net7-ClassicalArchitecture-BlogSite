c9f5b31723d4af4c210a4c694bf60d28b19ea33f
Serhat Kaçmaz <serhatkacmaz3@gmail.com>
2023-04-25 23:31:46 +0300

    paket version

--- src/Clients/BlogSite.Web/Controllers/HomeController.cs
+++ src/Clients/BlogSite.Web/Controllers/HomeController.cs
@@ -7,29 +7,10 @@ namespace BlogSite.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly AuthApiService _authApiService;
-
-        public HomeController(ILogger<HomeController> logger, AuthApiService authApiService)
-        {
-            _logger = logger;
-            _authApiService = authApiService;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
