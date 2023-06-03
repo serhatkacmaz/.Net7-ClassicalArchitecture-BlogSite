79c5d45e4dc6d84bae8e76118a08b7d320c274e5
Serhat Kaçmaz <serhatkacmaz3@gmail.com>
2023-06-03 12:54:21 +0300

    new migration

---
+++ src/Api/BlogSite.Repository/Migrations/20230603095155_ver004.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogSite.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class ver004 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 6, 3, 12, 51, 55, 412, DateTimeKind.Local).AddTicks(4727));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 6, 3, 12, 51, 55, 412, DateTimeKind.Local).AddTicks(4740));
+
+            migrationBuilder.UpdateData(
+                table: "UserRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 6, 3, 12, 51, 55, 412, DateTimeKind.Local).AddTicks(5052));
+
+            migrationBuilder.UpdateData(
+                table: "UserRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 6, 3, 12, 51, 55, 412, DateTimeKind.Local).AddTicks(5055));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "Image" },
+                values: new object[] { new DateTime(2023, 6, 3, 12, 51, 55, 412, DateTimeKind.Local).AddTicks(5175), new byte[] { 255, 216, 255, 224, 0, 16, 74, 70, 73, 70, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 255, 219, 0, 132, 0, 8, 6, 6, 7, 6, 5, 8, 7, 7, 7, 9, 9, 8, 10, 12, 20, 13, 12, 11, 11, 12, 25, 18, 19, 15, 20, 29, 26, 31, 30, 29, 26, 28, 28, 32, 36, 46, 39, 32, 34, 44, 35, 28, 28, 40, 55, 41, 44, 48, 49, 52, 52, 52, 31, 39, 57, 61, 56, 50, 60, 40, 51, 52, 50, 255, 219, 0, 132, 1, 9, 9, 9, 12, 11, 12, 24, 13, 13, 24, 50, 33, 28, 33, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50 } });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "Image" },
+                values: new object[] { new DateTime(2023, 6, 3, 12, 51, 55, 412, DateTimeKind.Local).AddTicks(5318), new byte[] { 255, 216, 255, 224, 0, 16, 74, 70, 73, 70, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 255, 219, 0, 132, 0, 8, 6, 6, 7, 6, 5, 8, 7, 7, 7, 9, 9, 8, 10, 12, 20, 13, 12, 11, 11, 12, 25, 18, 19, 15, 20, 29, 26, 31, 30, 29, 26, 28, 28, 32, 36, 46, 39, 32, 34, 44, 35, 28, 28, 40, 55, 41, 44, 48, 49, 52, 52, 52, 31, 39, 57, 61, 56, 50, 60, 40, 51, 52, 50, 255, 219, 0, 132, 1, 9, 9, 9, 12, 11, 12, 24, 13, 13, 24, 50, 33, 28, 33, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50 } });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 5, 13, 16, 17, 50, 101, DateTimeKind.Local).AddTicks(3594));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 5, 13, 16, 17, 50, 101, DateTimeKind.Local).AddTicks(3605));
+
+            migrationBuilder.UpdateData(
+                table: "UserRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 5, 13, 16, 17, 50, 101, DateTimeKind.Local).AddTicks(3840));
+
+            migrationBuilder.UpdateData(
+                table: "UserRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 5, 13, 16, 17, 50, 101, DateTimeKind.Local).AddTicks(3842));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "Image" },
+                values: new object[] { new DateTime(2023, 5, 13, 16, 17, 50, 101, DateTimeKind.Local).AddTicks(3947), null });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "Image" },
+                values: new object[] { new DateTime(2023, 5, 13, 16, 17, 50, 101, DateTimeKind.Local).AddTicks(3949), null });
+        }
+    }
+}
